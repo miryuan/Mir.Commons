c62f4393c302281c0e765764bf3b065ac5a24204
YuanZhenfeng <weivyuan@hotmail.com>
2018-12-03 12:11:37 +0800

    扩充辅助类

---
+++ Mir.Commons/Map/SpaceHelper.cs
@@ -0,0 +1,313 @@
+﻿/******************************************************************
+* 项目名称 ：Mir.Commons.Map 
+* 项目描述 ： 
+* 类 名 称 ：SpaceHelper 
+* 类 描 述 ： 
+* CLR 版本 ：4.0.30319.42000 
+* 版权所有 ：袁振峰
+* 联系方式 ：http://www.ustuy.com/ 
+******************************************************************/
+using System;
+
+namespace Mir.Commons.Map
+{
+    /// <summary>
+    /// 空间/坐标帮助类
+    /// </summary>
+    public class SpaceUtil
+    {
+        public static string BAIDU_LBS_TYPE { get { return "bd09ll"; } }
+
+        public static double pi = 3.1415926535897932384626433832795;
+        public static double a = 6378245.0;
+        public static double ee = 0.00669342162296594323;
+
+        /// <summary>
+        /// 数字经纬度和度分秒经纬度转换 (Digital degree of latitude and longitude and vehicle to latitude and longitude conversion)
+        /// </summary>
+        /// <param name="digitalLati_Longi">数字经纬度</param>
+        /// <return>度分秒经纬度</return>
+        public static string ConvertDigitalToDegrees(string digitalLati_Longi)
+        {
+            double digitalDegree = Convert.ToDouble(digitalLati_Longi);
+            return ConvertDigitalToDegrees(digitalDegree);
+        }
+
+        /// <summary>
+        /// 数字经纬度和度分秒经纬度转换 (Digital degree of latitude and longitude and vehicle to latitude and longitude conversion)
+        /// </summary>
+        /// <param name="digitalDegree">数字经纬度</param>
+        /// <return>度分秒经纬度</return>
+        public static string ConvertDigitalToDegrees(double digitalDegree)
+        {
+            const double num = 60;
+            int degree = (int)digitalDegree;
+            double tmp = (digitalDegree - degree) * num;
+            int minute = (int)tmp;
+            double second = (tmp - minute) * num;
+            string degrees = "" + degree + "°" + minute + "′" + second + "″";
+            return degrees;
+        }
+
+
+        /// <summary>
+        /// 度分秒经纬度(必须含有'°')和数字经纬度转换
+        /// </summary>
+        /// <param name="digitalDegree">度分秒经纬度</param>
+        /// <return>数字经纬度</return>
+        public static double ConvertDegreesToDigital(string degrees)
+        {
+            const double num = 60;
+            double digitalDegree = 0.0;
+            int d = degrees.IndexOf('°');           //度的符号对应的 Unicode 代码为：00B0[1]（六十进制），显示为°。
+            if (d < 0)
+            {
+                return digitalDegree;
+            }
+            string degree = degrees.Substring(0, d);
+            digitalDegree += Convert.ToDouble(degree);
+
+            int m = degrees.IndexOf('\'');           //分的符号对应的 Unicode 代码为：2032[1]（六十进制），显示为′。
+            if (m < 0)
+            {
+                return digitalDegree;
+            }
+            string minute = degrees.Substring(d + 1, m - d - 1);
+            digitalDegree += ((Convert.ToDouble(minute)) / num);
+
+            int s = degrees.IndexOf('\"');           //秒的符号对应的 Unicode 代码为：2033[1]（六十进制），显示为″。
+            if (s < 0)
+            {
+                return digitalDegree;
+            }
+            string second = degrees.Substring(m + 1, s - m - 1);
+            digitalDegree += (Convert.ToDouble(second) / (num * num));
+
+            return digitalDegree;
+        }
+
+
+        /// <summary>
+        /// 度分秒经纬度(必须含有'/')和数字经纬度转换
+        /// </summary>
+        /// <param name="digitalDegree">度分秒经纬度</param>
+        /// <param name="cflag">分隔符</param>
+        /// <return>数字经纬度</return>
+        public static double ConvertDegreesToDigital_default(string degrees)
+        {
+            char ch = '/';
+            return ConvertDegreesToDigital(degrees, ch);
+        }
+
+        /// <summary>
+        /// 度分秒经纬度和数字经纬度转换
+        /// </summary>
+        /// <param name="digitalDegree">度分秒经纬度</param>
+        /// <param name="cflag">分隔符</param>
+        /// <return>数字经纬度</return>
+        public static double ConvertDegreesToDigital(string degrees, char cflag)
+        {
+            const double num = 60;
+            double digitalDegree = 0.0;
+            int d = degrees.IndexOf(cflag);
+            if (d < 0)
+            {
+                return digitalDegree;
+            }
+            string degree = degrees.Substring(0, d);
+            digitalDegree += Convert.ToDouble(degree);
+
+            int m = degrees.IndexOf(cflag, d + 1);
+            if (m < 0)
+            {
+                return digitalDegree;
+            }
+            string minute = degrees.Substring(d + 1, m - d - 1);
+            digitalDegree += ((Convert.ToDouble(minute)) / num);
+
+            int s = degrees.Length;
+            if (s < 0)
+            {
+                return digitalDegree;
+            }
+            string second = degrees.Substring(m + 1, s - m - 1);
+            digitalDegree += (Convert.ToDouble(second) / (num * num));
+
+            return digitalDegree;
+        }
+
+        /// <summary>
+        /// 84 to 火星坐标系 (GCJ-02) World Geodetic System ==> Mars Geodetic System
+        /// </summary>
+        /// <param name="lat"></param>
+        /// <param name="lon"></param>
+        /// <returns></returns>
+        public static Gps GPS84_To_GCJ02(double lat, double lon)
+        {
+            if (IsOutOfChina(lat, lon))
+            {
+                return null;
+            }
+
+            double dLat = TransformLat(lon - 105.0, lat - 35.0);
+            double dLon = TransformLon(lon - 105.0, lat - 35.0);
+            double radLat = lat / 180.0 * pi;
+            double magic = Math.Sin(radLat);
+            magic = 1 - ee * magic * magic;
+            double sqrtMagic = Math.Sqrt(magic);
+            dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+            dLon = (dLon * 180.0) / (a / sqrtMagic * Math.Cos(radLat) * pi);
+            double mgLat = lat + dLat;
+            double mgLon = lon + dLon;
+            return new Gps(mgLat, mgLon);
+        }
+
+        /// <summary>
+        /// 火星坐标系 (GCJ-02) to 84
+        /// </summary>
+        /// <param name="lat"></param>
+        /// <param name="lon"></param>
+        /// <returns></returns>
+        public static Gps GCJ02_To_GPS84(double lat, double lon)
+        {
+            Gps gps = Transform(lat, lon);
+            double lontitude = lon * 2 - gps.Lng;
+            double latitude = lat * 2 - gps.Lat;
+            return new Gps(latitude, lontitude);
+        }
+
+        /// <summary>
+        /// 火星坐标系 (GCJ-02) 与百度坐标系 (BD-09) 的转换算法 将 GCJ-02 坐标转换成 BD-09 坐标
+        /// </summary>
+        /// <param name="gg_lat"></param>
+        /// <param name="gg_lon"></param>
+        /// <returns></returns>
+        public static Gps GCJ02_To_BD09(double gg_lat, double gg_lon)
+        {
+            double x = gg_lon, y = gg_lat;
+            double z = Math.Sqrt(x * x + y * y) + 0.00002 * Math.Sin(y * pi);
+            double theta = Math.Atan2(y, x) + 0.000003 * Math.Cos(x * pi);
+            double bd_lon = z * Math.Cos(theta) + 0.0065;
+            double bd_lat = z * Math.Sin(theta) + 0.006;
+            return new Gps(bd_lat, bd_lon);
+        }
+
+        /// <summary>
+        /// 火星坐标系 (GCJ-02) 与百度坐标系 (BD-09) 的转换算法 * * 将 BD-09 坐标转换成GCJ-02 坐标
+        /// </summary>
+        /// <param name="bd_lat"></param>
+        /// <param name="bd_lon"></param>
+        /// <returns></returns>
+        public static Gps BD09_To_GCJ02(double bd_lat, double bd_lon)
+        {
+            double x = bd_lon - 0.0065, y = bd_lat - 0.006;
+            double z = Math.Sqrt(x * x + y * y) - 0.00002 * Math.Sin(y * pi);
+            double theta = Math.Atan2(y, x) - 0.000003 * Math.Cos(x * pi);
+            double gg_lon = z * Math.Cos(theta);
+            double gg_lat = z * Math.Sin(theta);
+            return new Gps(gg_lat, gg_lon);
+        }
+
+        /// <summary>
+        /// (BD-09)-->GPS84
+        /// </summary>
+        /// <param name="bd_lat"></param>
+        /// <param name="bd_lon"></param>
+        /// <returns></returns>
+        public static Gps BD09_To_GPS84(double bd_lat, double bd_lon)
+        {
+            Gps gcj02 = BD09_To_GCJ02(bd_lat, bd_lon);
+            Gps map84 = GCJ02_To_GPS84(gcj02.Lat, gcj02.Lng);
+            return map84;
+
+        }
+
+        /// <summary>
+        /// 是否在中国之外
+        /// </summary>
+        /// <param name="lat"></param>
+        /// <param name="lon"></param>
+        /// <returns></returns>
+        public static bool IsOutOfChina(double lat, double lon)
+        {
+            if (lon < 72.004 || lon > 137.8347)
+                return true;
+            if (lat < 0.8293 || lat > 55.8271)
+                return true;
+            return false;
+        }
+
+        public static Gps Transform(double lat, double lon)
+        {
+            if (IsOutOfChina(lat, lon))
+            {
+                return new Gps(lat, lon);
+            }
+            double dLat = TransformLat(lon - 105.0, lat - 35.0);
+            double dLon = TransformLon(lon - 105.0, lat - 35.0);
+            double radLat = lat / 180.0 * pi;
+            double magic = Math.Sin(radLat);
+            magic = 1 - ee * magic * magic;
+            double sqrtMagic = Math.Sqrt(magic);
+            dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+            dLon = (dLon * 180.0) / (a / sqrtMagic * Math.Cos(radLat) * pi);
+            double mgLat = lat + dLat;
+            double mgLon = lon + dLon;
+            return new Gps(mgLat, mgLon);
+        }
+
+        public static double TransformLat(double x, double y)
+        {
+            double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y
+                    + 0.2 * Math.Sqrt(Math.Abs(x));
+            ret += (20.0 * Math.Sin(6.0 * x * pi) + 20.0 * Math.Sin(2.0 * x * pi)) * 2.0 / 3.0;
+            ret += (20.0 * Math.Sin(y * pi) + 40.0 * Math.Sin(y / 3.0 * pi)) * 2.0 / 3.0;
+            ret += (160.0 * Math.Sin(y / 12.0 * pi) + 320 * Math.Sin(y * pi / 30.0)) * 2.0 / 3.0;
+            return ret;
+        }
+
+        public static double TransformLon(double x, double y)
+        {
+            double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1
+                    * Math.Sqrt(Math.Abs(x));
+            ret += (20.0 * Math.Sin(6.0 * x * pi) + 20.0 * Math.Sin(2.0 * x * pi)) * 2.0 / 3.0;
+            ret += (20.0 * Math.Sin(x * pi) + 40.0 * Math.Sin(x / 3.0 * pi)) * 2.0 / 3.0;
+            ret += (150.0 * Math.Sin(x / 12.0 * pi) + 300.0 * Math.Sin(x / 30.0
+                    * pi)) * 2.0 / 3.0;
+            return ret;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Gps
+    {
+        public Gps(double wgLat, double wgLon)
+        {
+            this.Lat = wgLat;
+            this.Lng = wgLon;
+        }
+
+        /// <summary>
+        /// 经度
+        /// </summary>
+        /// <value></value>
+        public double Lat { get; }
+
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        /// <value></value>
+        public double Lng { get; }
+
+        /// <summary>
+        /// 返回经纬度("经度,纬度")
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString()
+        {
+            return Lng + "," + Lat;
+        }
+    }
+}
---
+++ Mir.Commons/Net/HttpHelper.cs
@@ -0,0 +1,129 @@
+﻿/******************************************************************
+* 项目名称 ：Mir.Commons.Net 
+* 项目描述 ： 
+* 类 名 称 ：HttpHelper 
+* 类 描 述 ： 
+* CLR 版本 ：4.0.30319.42000 
+* 版权所有 ：袁振峰
+* 联系方式 ：http://www.ustuy.com/ 
+******************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mir.Commons.Net
+{
+    /// <summary>
+    /// Http Post/Get
+    /// </summary>
+    public class HttpHelper
+    {
+        /// <summary>
+        /// 发起POST同步请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="postData">请求数据</param>
+        /// <param name="contentType">application/xml、application/json、application/text、application/x-www-form-urlencoded</param>
+        /// <param name="headers">填充消息头</param>        
+        /// <returns></returns>
+        public static string HttpPost(string url, string postData = null, string contentType = null, int timeOut = 30, Dictionary<string, string> headers = null)
+        {
+            postData = postData ?? "";
+            using (HttpClient client = new HttpClient())
+            {
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+                using (HttpContent httpContent = new StringContent(postData, Encoding.UTF8))
+                {
+                    if (contentType != null)
+                        httpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+
+                    HttpResponseMessage response = client.PostAsync(url, httpContent).Result;
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 发起POST异步请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="postData">请求数据</param>
+        /// <param name="contentType">application/xml、application/json、application/text、application/x-www-form-urlencoded</param>
+        /// <param name="headers">填充消息头</param>        
+        /// <returns></returns>
+        public static async Task<string> HttpPostAsync(string url, string postData = null, string contentType = null, int timeOut = 30, Dictionary<string, string> headers = null)
+        {
+            postData = postData ?? "";
+            using (HttpClient client = new HttpClient())
+            {
+                client.Timeout = new TimeSpan(0, 0, timeOut);
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+                using (HttpContent httpContent = new StringContent(postData, Encoding.UTF8))
+                {
+                    if (contentType != null)
+                        httpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+
+                    HttpResponseMessage response = await client.PostAsync(url, httpContent);
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 发起GET同步请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="contentType">application/xml、application/json、application/text、application/x-www-form-urlencoded</param>
+        /// <param name="headers">填充消息头</param>   
+        /// <returns></returns>
+        public static string HttpGet(string url, string contentType = null, Dictionary<string, string> headers = null)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                if (contentType != null)
+                    client.DefaultRequestHeaders.Add("ContentType", contentType);
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        /// <summary>
+        /// 发起GET异步请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="contentType">application/xml、application/json、application/text、application/x-www-form-urlencoded</param>
+        /// <param name="headers">填充消息头</param>   
+        /// <returns></returns>
+        public static async Task<string> HttpGetAsync(string url, string contentType = null, Dictionary<string, string> headers = null)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                if (contentType != null)
+                    client.DefaultRequestHeaders.Add("ContentType", contentType);
+                if (headers != null)
+                {
+                    foreach (var header in headers)
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+                HttpResponseMessage response = await client.GetAsync(url);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
