4d16dcd1ffb77bbd5338e02a4ee7b8f933496e57
YuanZhenfeng <weivyuan@hotmail.com>
2018-12-03 14:07:12 +0800

    去掉不常用部分的扩展

---
+++ UnitTest/UnitTest_MemoryCache.cs
@@ -0,0 +1,22 @@
+﻿/******************************************************************
+* 项目名称 ：UnitTest 
+* 项目描述 ： 
+* 类 名 称 ：UnitTest_MemoryCache 
+* 类 描 述 ： 
+* CLR 版本 ：4.0.30319.42000 
+* 版权所有 ：袁振峰
+* 联系方式 ：http://www.ustuy.com/ 
+******************************************************************/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest_MemoryCache
+    {
+
+    }
+}
